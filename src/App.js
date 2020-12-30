203eb5b1228f89d3dc2e230e0728b08b9c138cdb
petruki <31597636+petruki@users.noreply.github.com>
2020-12-30 01:13:58 -0800

    Fixing gh-pages issues

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ const App = () => {
       <React.Fragment>
         <NavbarComponent />
         <div className="container">
-          <Route path="/" exact component={HomeComponent} />
-          <Route path="/project/:id" exact component={ProjectStandaloneComponent} />
+          <Route path="/" component={HomeComponent} />
+          <Route exact path="/project/:id" component={ProjectStandaloneComponent} />
         </div>
       </React.Fragment>
       <FooterComponent />
